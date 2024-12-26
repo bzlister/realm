594ae2ea10b81dfa1c5b008bfcb5d830e39d67d2
Ben Lister <belister@microsoft.com>
2024-12-26 16:53:48 +0900

    good intro

--- app/(common)/profile-image.tsx
+++ app/(common)/profile-image.tsx
@@ -4,7 +4,7 @@ import "./profile-image.css";
 export default function ProfileImage() {
   return (
     <div className="profile-image">
-      <Image src="/images/profile_circle.png" alt="Personal photo" width={300} height={300} />
+      <Image src="/images/profile_circle.png" alt="Personal photo" width={320} height={320} />
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,27 @@ export default function Home() {
   return (
     <div className="page-container">
       <div>
-        <div className="intro-container">
+        <div className="hero-container">
           <ProfileImage />
           <div className="intro">
             <Typography level="h1">
               Hey👋 I'm <Typography className="attention">Ben Lister</Typography>
             </Typography>
-            <h3>Full-stack developer</h3>
+            <h3>Full-stack developer based in the D.C. metro area</h3>
           </div>
         </div>
         <div id="triangle"></div>
       </div>
       <div className="details-container">
-        {Array(60)
-          .fill(0)
-          .map((_, i) => [i, <br />])
-          .flat()}
+        <h2>About me</h2>
+        <p>
+          I think the act of programming is sublime.
+          It is the process of expressing an idea under the engineering system constraints, while optimizing for a weighted combination of performance, simplicity, scalability, and development time.
+          I go through this process to create apps and websites with responsive user experiences and backend systems that are fast and reliable.
+        </p>
+        <p>
+          Outside of my work, you can find me planning my next Path of Exile build, or hiking in Shenendoah listening to a sci-fi/fantasy audiobook.
+        </p>
       </div>
     </div>
   );
