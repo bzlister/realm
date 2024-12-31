64da27775038294934663d3c649cef82102dfacc
bzlis <bzlis@github.com>
2024-12-31 07:29:07 -0500

    settled on experience card content design. Added tags

--- app/(common)/tags.tsx
+++ app/(common)/tags.tsx
@@ -1,3 +1,6 @@
-export default function Tags({ tags }: { tags: string[]}) {
-    return <div />;
+import { Chip } from "@mui/joy";
+import "./tags.css";
+
+export default function Tags({ tags }: { tags: string[] }) {
+  return <div>{...tags.map((t) => <Chip variant="soft">{t}</Chip>)}</div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -23,9 +23,10 @@ export default function Home() {
         <div className="details-section">
           <h2>About me</h2>
           <p>
-            I think programming is sublime.
-            It is the process of expressing an idea using the tools offered by the system, while optimizing for a weighted combination of performance, simplicity, scalability, and development time.
-            I iterate through this process to create apps and websites with <span className="attention">responsive user experiences</span> and backend systems that are <span className="attention">fast</span> and <span className="attention">reliable</span>.
+            I think programming is sublime. It is the process of expressing an idea using the tools offered by the system, while optimizing for a weighted
+            combination of performance, simplicity, scalability, and development time. I iterate through this process to create apps and websites with{" "}
+            <span className="attention">responsive user experiences</span> and backend systems that are <span className="attention">fast</span> and{" "}
+            <span className="attention">reliable</span>.
           </p>
           <p>
             Outside of my work, you can find me planning my next Path of Exile build, or hiking in Shenandoah while listening to a sci-fi/fantasy audiobook.
@@ -35,17 +36,24 @@ export default function Home() {
           <h2>Experience</h2>
           <div className="experiences-container">
             <Experience title="Microsoft - Software Engineer II" startYear="2020" endYear="Present" defaultExpanded>
-              <ul>
-                <li><span>2022 - Present (SWE 2):</span>Developed features for pro users of Power BI desktop and service. Shipped a new UX in the model view. Lead developer in adding 1st-party support for calculation group authoring and for creating Direct Lake semantic models.</li>
-                <li><span>2020 - 2022 (SWE 1):</span>Developed features for the M365 Signup e-commerce platform. Built internal scenario testing framework. Lead developer in a UX redesign effort.</li>
-              </ul>
-              <Tags tags={["Angular", "React", "C#", "Typescript", "Redux"]}/>
+              <div className="msft">
+                <p>
+                  Developed features for pro users of Power BI desktop and service. Lead developer for several projects including a new model view UX, adding
+                  1st-party support for creating calculation groups, and the client integration for creating Direct Lake semantic models.
+                </p>
+                <p>
+                  In an earlier role, I created and maintained features for the M365 Signup e-commerce platform. Built internal scenario testing framework. Lead
+                  developer in a UX redesign effort.
+                </p>
+              </div>
+              <Tags tags={["Angular", "React", "C#", "Typescript", "Redux"]} />
             </Experience>
             <Experience title="Microsoft - Software Engineer Intern" startYear="2019">
-              <Tags tags={["C#", "C++", "Azure"]} />
+              <p>Prototyped a pipeline for managing and assigning gig workloads. Fixed a minor bug in the Windows 11 out-of-box experience.</p>
+              <Tags tags={["C#", "C++"]} />
             </Experience>
             <Experience title="CARFAX - Software Engineer Intern" startYear="2018">
-              Learned agile practices. Built an internal tool for detecting deprecated business logic.
+              <p>Learned agile practices. Built an internal tool for detecting deprecated config files.</p>
               <Tags tags={["Java"]} />
             </Experience>
           </div>
