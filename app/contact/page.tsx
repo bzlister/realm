a1e5bda64b89d3742dac6fbd62d9b740db4d8b67
bzlis <bzlister@gmail.com>
2025-01-19 20:37:34 -0500

    removed unused header in contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,9 +8,6 @@ import "./page.css";
 export default function Contact() {
   return (
     <div>
-      {/* <div className="contact-header">
-        <h2>Let's build something together.</h2>
-      </div> */}
       <div className="contact-container">
         <ContactForm />
         <div className="socials">
